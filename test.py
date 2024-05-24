ac38d18b06ae38f47a64cad88a664f071deeb375
Thayse Pacheco <thayse.pacheco@usp.br>
2024-05-24 17:48:44 -0300

    test script

---
+++ test.py
@@ -0,0 +1,7 @@
+import src.tap_magnitudes.magnitudeFactory as mag
+
+magFact = mag.HSTMagnitudeFactory()
+magFact.load_spectrum('NGC2808_total_ssp.dat')
+magFact.compute_magnitude()
+
+print(magFact.magnitudes)
